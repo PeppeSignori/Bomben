67aa92ffee43a34a11b4ef47250332b5e171f359
PeppeSignori <magnusson_erik@hotmail.com>
2015-03-08 15:40:38 +0100

    8 mars

--- UnitTestProject1/MatchTests.cs
+++ UnitTestProject1/MatchTests.cs
@@ -14,10 +14,11 @@ namespace UnitTestProject1
             //Arrange 
             var match = new Match();
             //Act
-            double resultat = match.beräknaFörväntadMålantal();
+            double resultat = match.beräknaFörväntadMålantal(2.5, 0.01, 1.80, 1.80, 1.75, 1.85);
             //Assert
             double förväntatSvar = 3;
             Assert.AreEqual(förväntatSvar, resultat);
         }
     }
 }
+
