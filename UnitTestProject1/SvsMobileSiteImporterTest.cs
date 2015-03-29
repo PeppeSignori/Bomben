470881288d86b298836d3d8aef008322a3c3b5f6
SigmaLambda <christer.lindberg@live.se>
2015-03-29 22:29:32 +0200

    No changes

---
+++ UnitTestProject1/SvsMobileSiteImporterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bomben;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class SvsMobileSiteImporterTest
+    {
+        [TestMethod]
+        public void GetInfoTest()
+        {
+            //Arrange
+            var Bombs = new SvsMobileSiteImporterTest();
+            //Act
+            
+            //Assert
+        }
+    }
+}
